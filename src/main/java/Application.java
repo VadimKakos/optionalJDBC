9e64371f9749d7c50719b6b04b0540d87e0c3480
vadimkosolapov <vilgutis@icloud.com>
2023-07-10 19:16:32 +0500

    done

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,5 +6,8 @@ public class Application {
         System.out.println(employeeDAO.findById());
         System.out.println(employeeDAO.getAllEmployee());
         System.out.println(employeeDAO.createEmployee(new Employee("Vad", "kos", "m", "pet", 2)));
+        Employee employee = new Employee("Вадим", "Кос", "М", "Санкт-Петербург", 2);
+        System.out.println(employeeDAO.updateEmployee(2, employee));
+        employeeDAO.deleteEmployee(2);
     }
 }
